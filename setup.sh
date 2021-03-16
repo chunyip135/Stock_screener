ee693f5acfa2dcadbd3c35f7e83b5ce9610a899c
chunyip135 <41230716+chunyip135@users.noreply.github.com>
2021-03-16 21:11:06 +0800

    2nd

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-	[general]\n\
-	email = \chunyip246@gmail.com\"\n\
-	" > ~/.streamlit/credentials.toml
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
-	[server]\n\
-	headless = true\n\
-	enableCORS=false\n\
-	port = $PORT\n\
-	" > ~/.streamlit/config.toml
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
